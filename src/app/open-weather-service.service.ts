d3591ad4052038e4e3b2a8aa4721e7bf0a10bf73
unknown <duduhayo@gmail.com>
2018-05-12 14:15:36 +0300

    deploy hakuro

--- src/app/open-weather-service.service.ts
+++ src/app/open-weather-service.service.ts
@@ -16,7 +16,7 @@ export class OpenWeatherService {
   }
   public getBboxData(longLeft,latBottom,longRight,latTop,zoom)
   {
-    const url = `http://api.openweathermap.org/data/2.5/box/city?bbox=${longLeft},${latBottom},${longRight},${latTop},${zoom}&appid=9ebe67e1eb2ee9236eb779697cd4035e`;
+    const url = `https://api.openweathermap.org/data/2.5/box/city?bbox=${longLeft},${latBottom},${longRight},${latTop},${zoom}&appid=9ebe67e1eb2ee9236eb779697cd4035e`;
     let response:any = {};
     return this.client.get(url);
   }
